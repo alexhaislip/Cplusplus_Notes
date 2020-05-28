7730f253ce8dd27ff4dd0719e6e33c1dc6ea96dc
alexhaislip <haislipalexander@gmail.com>
2020-05-28 01:57:48 -0500

    updated code and fixed an issue

--- weight_Calculator/weight_calculator.cpp
+++ weight_Calculator/weight_calculator.cpp
@@ -1,42 +1,62 @@
+//©Alexander Haislip, Wednesday March 27th 2020 
 #include <iostream>
 #include <string>
 using namespace std;
+
+// Function that calculates your weight on another planet
+double calc(double earthWeight, double rel) {
+  return earthWeight * rel;
+}
 
 int main() {
 
   double planet_Weight;
   double earth_Weight;
-  double calc = earth_Weight * planet_Weight;
 
+//This will take input from the user asking their current earth weight.
   cout << "What is your weight on earth? LBS : ";
-  cin.clear();//you need to clear the flags before input
-  getline (cin, earth_Weight);
+  cin >> earth_Weight;
 
+  int planet;
+  std::cout << "Choose a planet in which you want to know your weight on :\n";
+  std::cout << "Mars => 1\nVenus => 2\nJupiter => 3\nSaturn	=> 4\nUranus => 5\nNeptune => 6\n";
+  cin >> planet;
 
-  std::cout << "Choose a relative gravity for the planet in which you want to know your weight on : Planet Relative Gravity Earth = 1 , Venus	= 0.78 , Mars	= 0.39 , Jupiter	= 2.65 , Saturn	= 1.17 , Uranus	= 1.05 , Neptune	= 1.23:   \n";
-  cin.clear();//you need to clear the flags before input
-  getline (cin, planet_Weight);
-
+  double planetMultiplier;
+  string planetName;
   
-  if (planet_Weight == 1) {
-    cout << "Your weight on Earth is... " << calc; 
-  } else if (planet_Weight == 0.39) {
-    cout << "Your weight on Mars is... " << calc; 
-  } else if (planet_Weight == 0.78) {
-    cout << "Your weight on Venus is... " << calc; 
-  } else if (planet_Weight == 2.65) {
-    cout << "Your weight on Jupiter is... " << calc; 
-  } else if (planet_Weight == 1.17) {
-    cout << "Your weight on Saturn is... " << calc; 
-  } else if (planet_Weight == 1.05) {
-    cout << "Your weight on Uranus is... " << calc; 
-  } else if (planet_Weight == 1.23) {
-    cout << "Your weight on Neptune is... " << calc; 
-  } else { 
+  switch(planet) {
+    case 1 : // Mars
+      planetMultiplier = 0.78;
+      planetName = "Mars";
+      break;
+    case 2 : // Venus
+      planetMultiplier = 2.65;
+      planetName = "Venus";
+      break;
+    case 3 : // Jupiter
+      planetMultiplier = 1.17;
+      planetName = "Jupiter";
+      break; 
+    case 4 : // Saturn
+      planetMultiplier = 1.05;
+      planetName = "Saturn";
+      break;
+    case 5 : // Uranus
+      planetMultiplier = 1.23;
+      planetName = "Uranus";
+      break;
+    case 6 : // Neptune
+      planetMultiplier = 0.17;
+      planetName = "Neptune";
+      break;
 
-    cout << "Your weight on this planet is Unknown... "; 
 
-    
   }
-  
+
+  double newWeight = calc(earth_Weight, planetMultiplier);
+  cout << "Your weight on " << planetName << " is " << newWeight << endl;
+   
 }
+  
+
